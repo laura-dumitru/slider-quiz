b5e5c65538f2a0fd5cc281d18648eb8273bd9ac9
Laura Dumitru <lauradumitru88@icloud.com>
2024-01-30 12:56:00 +0000

    Added separate line height for each line

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// should have a small hand that animates across the slider then disappears
 //different font
-// make the lines move up and have different height
-// reset % back to 0
 // responsive
-
-//console.log(gsap);
+//animation to run and slider disabled until animation finished
 
 const answers = ["6 hrs", "7 hrs", "8 hrs", "9 hrs"];
 const correctAnswerIndex = 2;
 const correctAnswer = answers[correctAnswerIndex];
-const column = document.querySelectorAll(".column");
-let percents = document.querySelectorAll(".percent");
+
+const heights = [60, 80, 100, 80];
+
+const columns = document.querySelectorAll(".column");
+const percents = document.querySelectorAll(".percent");
 
 const slider = document.getElementById("slider");
-const lines = document.querySelectorAll(".column");
 
 const button = document.getElementById("reset");
 
@@ -22,7 +20,7 @@ const correctAnswerText = document.querySelector(".correct-answer");
 
 const pointer = document.querySelector(".lucide.lucide-pointer");
 
-// linesArray.length = [0-3] (4)
+// columnsArray.length = [0-3] (4)
 // event.target.value [1-100] (%)
 
 function pulse() {
@@ -38,7 +36,16 @@ function moveRight() {
   gsap.to(pointer, { x: "+=630", duration: 1.5, onComplete: moveBack, ease: Power1.easeInOut });
 }
 function moveBack() {
-  gsap.to(pointer, { x: "-=630", duration: 1.5, ease: Power1.easeInOut });
+  gsap.to(pointer, {
+    x: "-=630",
+    duration: 1.5,
+    ease: Power1.easeInOut,
+    onComplete: iconDisappear,
+  });
+}
+// Define the icon disappear animation
+function iconDisappear() {
+  gsap.to(pointer, { opacity: 0, duration: 2 });
 }
 
 function mapRange(inputMin, inputMax, outputMin, outputMax, value) {
@@ -48,23 +55,19 @@ function mapRange(inputMin, inputMax, outputMin, outputMax, value) {
 //slider.style.transition = "value 10s ease-in-out";
 
 slider.addEventListener("input", function (event) {
-  const linesArray = Array.from(lines);
+  const columnsArray = Array.from(columns);
   //console.log(event);
   const mapResult = Math.round(
-    mapRange(event.target.min, event.target.max, 0, linesArray.length - 1, event.target.value)
+    mapRange(event.target.min, event.target.max, 0, columnsArray.length - 1, event.target.value)
   );
   const currentAnswer = answers[mapResult];
   console.log(mapResult);
 
   //console.log(event.target.value);
-  Array.from(column).forEach((column) => {
-    //column.style.position = "relative";
-    //column.style.top = "-1px";
-    lines.forEach((line) => {
-      line.style.height = "120px";
-      line.style.backgroundColor = "#8252AB";
-      slider.style.backgroundColor = "#8252AB";
-    });
+  Array.from(columns).forEach((column, i) => {
+    column.style.height = `${heights[i]}px`;
+    column.style.backgroundColor = "#8252AB";
+    slider.style.backgroundColor = "#8252AB";
   });
 
   //console.log(mapResult);
@@ -74,13 +77,13 @@ slider.addEventListener("input", function (event) {
   } else {
     console.log("You got it wrong!");
     // snap to correct answer with delay
-    // const snapPoints = [10, 37, 64, 90];
-    //mapRange(0, linesArray.length - 1, event.target.min, event.target.max, correctAnswerIndex);
+    // const snapPoints = [10, 3s7, 64, 90];
+    //mapRange(0, columnsArray.length - 1, event.target.min, event.target.max, correctAnswerIndex);
 
     const offset = 0.1;
     const reverseMap = mapRange(
       0,
-      linesArray.length - 1,
+      columnsArray.length - 1,
       event.target.min,
       event.target.max,
       correctAnswerIndex - offset
@@ -92,10 +95,10 @@ slider.addEventListener("input", function (event) {
     }, 1000);
   }
 
-  userSelectedText.innerHTML = `You selected <strong>${currentAnswer}</strong>.`;
+  userSelectedText.innerHTML = `You selected: <strong>${currentAnswer}</strong>.`;
   correctAnswerText.innerHTML = `The correct answer is: <strong>${correctAnswer}</strong>.`;
 
-  const percentages = ["9%", "20%", "39%", "32%"];
+  const percentages = ["15%", "20%", "35%", "30%"];
 
   percents.forEach((percent, index) => {
     percent.style.opacity = "1";
@@ -103,13 +106,17 @@ slider.addEventListener("input", function (event) {
   });
 
   button.addEventListener("click", function () {
-    console.log("click");
     slider.value = 0;
     slider.style.backgroundColor = "#8e7dbe";
+    userSelectedText.innerHTML = `You selected:`;
+    correctAnswerText.innerHTML = `The correct answer is:`;
 
-    lines.forEach((line) => {
-      line.style.backgroundColor = "#8e7dbe";
-      line.style.height = "50px";
+    columns.forEach((column) => {
+      column.style.backgroundColor = "#8e7dbe";
+      column.style.height = "40px";
+    });
+    percents.forEach((percent, index) => {
+      percent.style.opacity = "0";
     });
   });
 });
