fe46aa144bc728c6875757edb841205ff774f481
Laura Dumitru <lauradumitru88@icloud.com>
2024-01-31 13:51:57 +0000

    Added snapping animation

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ const mobile = window.innerWidth < 786; //640
 // columnsArray.length = [0-3] (4)
 // event.target.value [1-100] (%)
 
-let animationFinished = false;
-
 const ease = Power1.easeInOut;
 
 function startAnimation() {
@@ -53,8 +51,7 @@ function startAnimation() {
   }
   // Define the icon disappear animation
   function iconDisappear() {
-    gsap.to(pointer, { opacity: 0, duration: 2, onComplete: () => (animationFinished = true) });
-    addEventListener();
+    gsap.to(pointer, { opacity: 0, duration: 2 });
     slider.disabled = false;
   }
 }
@@ -70,78 +67,85 @@ function mapRange(inputMin, inputMax, outputMin, outputMax, value) {
 
 //slider.style.transition = "value 10s ease-in-out";
 //if animation finished running add slider event listener
-function addEventListener() {
-  slider.addEventListener("input", function (event) {
-    const columnsArray = Array.from(columns);
-    //console.log(event);
-    const mapResult = Math.round(
-      mapRange(event.target.min, event.target.max, 0, columnsArray.length - 1, event.target.value)
+
+slider.addEventListener("input", function (event) {
+  const columnsArray = Array.from(columns);
+  //console.log(event);
+  const mapResult = Math.round(
+    mapRange(event.target.min, event.target.max, 0, columnsArray.length - 1, event.target.value)
+  );
+  const currentAnswer = answers[mapResult];
+  //console.log(mapResult);
+
+  //console.log(event.target.value);
+  Array.from(columns).forEach((column, i) => {
+    column.style.height = `${heights[i]}px`;
+    column.style.transition = " 0.7s ease-in-out";
+    column.style.backgroundColor = "#783f8e";
+    slider.style.backgroundColor = "#783f8e";
+  });
+
+  if (currentAnswer === correctAnswer) {
+    slideText.innerHTML = "You got it right!";
+    slideText.style.color = "#783f8e";
+
+    //console.log("You got it right!");
+  } else {
+    slideText.innerHTML = "You got it wrong, try again!";
+    slideText.style.color = "#783f8e";
+    //console.log("You got it wrong!");
+
+    const reverseMap = mapRange(
+      0,
+      columnsArray.length - 1,
+      event.target.min,
+      event.target.max,
+      correctAnswerIndex
     );
-    const currentAnswer = answers[mapResult];
-    //console.log(mapResult);
 
-    //console.log(event.target.value);
-    Array.from(columns).forEach((column, i) => {
-      column.style.height = `${heights[i]}px`;
-      column.style.transition = " 0.7s ease-in-out";
-      column.style.backgroundColor = "#783f8e";
-      slider.style.backgroundColor = "#783f8e";
-    });
+    const margin = mobile ? 0 : 4;
 
-    if (currentAnswer === correctAnswer) {
-      slideText.innerHTML = "You got it right!";
-      slideText.style.color = "#783f8e";
-      //slideText.style.fontSize = "1.5em";
-
-      //console.log("You got it right!");
-    } else {
-      slideText.innerHTML = "You got it wrong, try again!";
-      slideText.style.color = "#783f8e";
-      //slideText.style.fontSize = "1.5em";
-      //console.log("You got it wrong!");
-
-      const reverseMap = mapRange(
-        0,
-        columnsArray.length - 1,
-        event.target.min,
-        event.target.max,
-        correctAnswerIndex
-      );
-
-      const margin = mobile ? 0 : 4;
-
-      setTimeout(() => {
-        //slider.value = snapPoints[correctAnswerIndex];
-        slider.value = reverseMap - margin;
-      }, 1000);
+    function handleAnimation() {
+      gsap.to(slider, {
+        value: reverseMap - margin,
+        duration: 1, // Adjust duration as needed
+        delay: 1.5,
+        ease: Power1.easeInOut,
+      });
     }
+    handleAnimation();
+    /*
+    setTimeout(() => {
+      //slider.value = snapPoints[correctAnswerIndex];
+      slider.value = reverseMap - margin;
+    }, 1000);*/
+  }
 
-    userSelectedText.innerHTML = `You selected: <em>${currentAnswer}</em>.`;
-    correctAnswerText.innerHTML = `The correct answer is: <em>${correctAnswer}</em>.`;
+  userSelectedText.innerHTML = `You selected: <em>${currentAnswer}</em>.`;
+  correctAnswerText.innerHTML = `The correct answer is: <em>${correctAnswer}</em>.`;
 
-    const percentages = ["15%", "20%", "35%", "30%"];
+  const percentages = ["15%", "20%", "35%", "30%"];
 
-    percents.forEach((percent, index) => {
-      percent.style.opacity = "1";
-      percent.innerHTML = percentages[index];
+  percents.forEach((percent, index) => {
+    percent.style.opacity = "1";
+    percent.innerHTML = percentages[index];
+  });
+
+  button.addEventListener("click", function () {
+    slider.value = 0;
+    slider.style.backgroundColor = "#8252ab";
+    userSelectedText.innerHTML = `You selected:`;
+    correctAnswerText.innerHTML = `The correct answer is:`;
+    slideText.innerHTML = "Slide to record your answer:";
+    slideText.style.color = "initial";
+
+    columns.forEach((column) => {
+      column.style.backgroundColor = "#8252ab";
+      column.style.height = "40px";
+      column.style.transition = "none";
     });
-
-    button.addEventListener("click", function () {
-      slider.value = 0;
-      slider.style.backgroundColor = "#8252ab";
-      userSelectedText.innerHTML = `You selected:`;
-      correctAnswerText.innerHTML = `The correct answer is:`;
-      slideText.innerHTML = "Slide to record your answer:";
-      slideText.style.color = "initial";
-
-      columns.forEach((column) => {
-        column.style.backgroundColor = "#8252ab";
-        column.style.height = "40px";
-        column.style.transition = "none";
-      });
-      percents.forEach((percent, index) => {
-        percent.style.opacity = "0";
-      });
+    percents.forEach((percent, index) => {
+      percent.style.opacity = "0";
     });
   });
-}
+});
